fc03cc668f075c8a78d47d08d420bf9b520a6f1c
Nemanja Zbiljić <nemanja.zbiljic@sbgenomics.com>
2016-02-16 17:11:38 +0100

    Expanded star import

--- src/main/java/com/github/zbiljic/shiro/cache/infinispan/InfinispanCache.java
+++ src/main/java/com/github/zbiljic/shiro/cache/infinispan/InfinispanCache.java
@@ -29,7 +29,11 @@ import org.apache.shiro.util.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Shiro {@link org.apache.shiro.cache.Cache} implementation that wraps an {@link org.infinispan.Cache} instance.
